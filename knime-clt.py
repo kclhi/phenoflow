ed17e49312f21bcda8b1d1f991982003429c7a1d
Martin Chapman <contact@martinchapman.co.uk>
2020-03-06 01:01:52 +0000

    Docker integration -- KNIME docker image -- CLT generation for running KNIME modules through image (pending issue with cwlgen handling of docker output dir) -- CLT generation for passing inputs and outputs -- Workflow generation

--- knime-clt.py
+++ knime-clt.py
@@ -1,30 +1,40 @@
 import cwlgen
 
-def createKnimeModule(label, inputEdge):
+def createKnimeModule(label, workflowInputEdge, dataInputEdge, type):
 
-    cwl_tool = cwlgen.CommandLineTool(tool_id=label.lower().replace(" ", "-"), label=label.lower().replace(" ", "-"), base_command='knime');
+    cwl_tool = cwlgen.CommandLineTool(tool_id=label.lower().replace(" ", "-"), label=label.lower().replace(" ", "-"), base_command='/home/kclhi/knime_4.1.1/knime');
 
-    # cwl_tool_docker = cwlgen.DockerRequirement(docker_pull = "knime:latest");
+    cwl_tool.namespaces.s = "https://phekb.org/";
+
+    # ~MDC Below not supported by visualiser?
+    cwl_tool_docker = cwlgen.DockerRequirement(docker_pull = "kclhi/knime:amia", docker_output_dir = "/home/kclhi/.eclipse");
     # Assume run in Docker
-    # cwl_tool.hints = cwl_tool_docker;
+    cwl_tool.requirements.append(cwl_tool_docker);
 
-    cwl_tool.arguments = ['-reset', '-nosplash', '-application', 'org.knime.product.KNIME_BATCH_APPLICATION'];
+    cwl_tool.arguments = ['-data', '/home/kclhi/.eclipse', '-reset', '-nosplash', '-nosave', '-application', 'org.knime.product.KNIME_BATCH_APPLICATION'];
 
-    file_binding = cwlgen.CommandLineBinding(prefix="-" + inputEdge + "=", separate=False);
+    workflow_file_binding = cwlgen.CommandLineBinding(prefix="-workflowFile=", separate=False);
 
-    input_file = cwlgen.CommandInputParameter(inputEdge, param_type='File', input_binding=file_binding, doc=inputEdge);
-    cwl_tool.inputs.append(input_file)
+    workflow_input_file = cwlgen.CommandInputParameter(workflowInputEdge, param_type='File', input_binding=workflow_file_binding, doc=workflowInputEdge);
+    cwl_tool.inputs.append(workflow_input_file)
 
-    output = cwlgen.CommandOutputParameter('output', doc='output of workflow run', param_type="stdout")
+    data_file_binding = cwlgen.CommandLineBinding(prefix="-workflow.variable=dm_potential_cases,file://", separate=False, value_from=" $(inputs.potentialCases.path),String");
+
+    data_input_file = cwlgen.CommandInputParameter(dataInputEdge, param_type='File', input_binding=data_file_binding, doc=dataInputEdge);
+    cwl_tool.inputs.append(data_input_file)
+
+    workflow_output_binding = cwlgen.CommandOutputBinding(glob="*.csv");
+
+    output = cwlgen.CommandOutputParameter('output', doc='output of KNIME module', param_type="File", output_binding=workflow_output_binding)
     cwl_tool.outputs.append(output)
 
     cwl_tool.doc = label
-    metadata = {'name': 'knime', 'about' : label}
+    metadata = {'type': type}
     cwl_tool.metadata = cwlgen.Metadata(**metadata)
 
     cwl_tool.export(outfile="output/" + label.lower().replace(" ", "-") + ".cwl");
 
-createKnimeModule("Read potential cases", "knimeModule");
-createKnimeModule("Abnormal lab", "knimeModule");
-createKnimeModule("Medication prescribed and abnormal lab", "knimeModule");
-createKnimeModule("TBC", "knimeModule");
+createKnimeModule("Read potential cases", "knimeModule", "potentialCases", "load");
+createKnimeModule("Abnormal lab", "knimeModule", "potentialCases", "logic");
+#createKnimeModule("Medication prescribed and abnormal lab", "knimeModule", "potentialCases", "logic");
+#createKnimeModule("TBC", "knimeModule", "potentialCases", "logic");
--- knime-workflow.py
+++ knime-workflow.py
@@ -11,23 +11,20 @@ def createKnimeWorkflow():
     knime_workflow.inputs.append(workflow_input);
 
     file_binding = cwlgen.CommandLineBinding();
+    workflow_input = cwlgen.InputParameter('potentialCases1', param_type='File', input_binding=file_binding, doc='potential cases file');
+    knime_workflow.inputs.append(workflow_input);
+
+    file_binding = cwlgen.CommandLineBinding();
     workflow_input = cwlgen.InputParameter('knimeModule2', param_type='File', input_binding=file_binding, doc='data pipeline file');
     knime_workflow.inputs.append(workflow_input);
 
-    file_binding = cwlgen.CommandLineBinding();
-    workflow_input = cwlgen.InputParameter('knimeModule3', param_type='File', input_binding=file_binding, doc='data pipeline file');
-    knime_workflow.inputs.append(workflow_input);
-
-    file_binding = cwlgen.CommandLineBinding();
-    workflow_input = cwlgen.InputParameter('knimeModule4', param_type='File', input_binding=file_binding, doc='data pipeline file');
-    knime_workflow.inputs.append(workflow_input);
-
     # Not necessary at present:
-    workflow_output = cwlgen.WorkflowOutputParameter('output', output_binding=cwlgen.CommandOutputBinding(output_eval="${}"), doc='output of full CWL Knime workflow', param_type="string")
+    workflow_output = cwlgen.WorkflowOutputParameter('output', output_binding=cwlgen.CommandOutputBinding(output_eval="${}"), doc='output of KNIME data pipeline', param_type="string")
     #knime_workflow.outputs.append(workflow_output)
 
     workflow_step_1 = cwlgen.WorkflowStep("1", "read-potential-cases.cwl");
     workflow_step_1.inputs.append(cwlgen.WorkflowStepInput("knimeModule", "knimeModule1"))
+    workflow_step_1.inputs.append(cwlgen.WorkflowStepInput("potentialCases", "potentialCases1"))
     workflow_step_1.out.append(cwlgen.WorkflowStepOutput("output"));
 
     workflow_step_2 = cwlgen.WorkflowStep("2", "abnormal-lab.cwl");
@@ -35,19 +32,10 @@ def createKnimeWorkflow():
     workflow_step_2.inputs.append(cwlgen.WorkflowStepInput("potentialCases", source="1/output"))
     workflow_step_2.out.append(cwlgen.WorkflowStepOutput("output"));
 
-    workflow_step_3 = cwlgen.WorkflowStep("3", "medication-prescribed-and-abnormal-lab.cwl");
-    workflow_step_3.inputs.append(cwlgen.WorkflowStepInput("knimeModule", "knimeModule3"))
-    workflow_step_3.inputs.append(cwlgen.WorkflowStepInput("identifiedAbnormalLab", source="2/output"))
-    workflow_step_3.out.append(cwlgen.WorkflowStepOutput("output"));
-
-    workflow_step_4 = cwlgen.WorkflowStep("4", "tbc.cwl");
-    workflow_step_4.inputs.append(cwlgen.WorkflowStepInput("knimeModule", "knimeModule4"))
-    workflow_step_4.inputs.append(cwlgen.WorkflowStepInput("identifiedPrescriptions", source="3/output"))
-    workflow_step_4.out.append(cwlgen.WorkflowStepOutput("output"));
-
-    knime_workflow.steps = knime_workflow.steps + [workflow_step_1, workflow_step_2, workflow_step_3, workflow_step_4];
+    knime_workflow.steps = knime_workflow.steps + [workflow_step_1, workflow_step_2];
 
     workflowId = "w" + str(uuid.uuid1());
+
     knime_workflow.export(outfile="output/" + workflowId + ".cwl");
 
     return workflowId;
@@ -73,5 +61,5 @@ def addWorkflowToViewer(workflowFile):
     print("Response: " + r.text)
 
 workflowId = createKnimeWorkflow();
-commitPushWorkflowRepo();
+#commitPushWorkflowRepo();
 #addWorkflowToViewer(workflowId + ".cwl");
