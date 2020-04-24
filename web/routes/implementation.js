997094041f417d03f4b90944f37c79c79b026308
Martin Chapman <contact@martinchapman.co.uk>
2020-04-24 14:19:05 +0100

    View uploaded implementations.

--- web/routes/implementation.js
+++ web/routes/implementation.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const logger = require('../config/winston');
-const models = require('../models');
-const sanitizeHtml = require('sanitize-html');
-const jwt = require('express-jwt');
+const logger = require("../config/winston");
+const models = require("../models");
+const sanitizeHtml = require("sanitize-html");
+const jwt = require("express-jwt");
 const config = require("config");
+const fs = require("fs")
 
-router.post('/:stepId/:language', jwt({secret:config.get("jwt.RSA_PRIVATE_KEY"), algorithms:['RS256']}), async function(req, res, next) {
+router.post("/:stepId/:language", jwt({secret:config.get("jwt.RSA_PRIVATE_KEY"), algorithms:["RS256"]}), async function(req, res, next) {
 
-  if(!req.files || Object.keys(req.files).length === 0) return res.status(400).send('No files were uploaded.');
+  if(!req.files || Object.keys(req.files).length === 0) return res.status(400).send("No files were uploaded.");
 
   let uploadedFile = req.files.implementation;
   let step;
@@ -33,6 +34,37 @@ router.post('/:stepId/:language', jwt({secret:config.get("jwt.RSA_PRIVATE_KEY"),
     }
 
   });
+
+});
+
+router.get("/:workflowId/:position/:language", async function(req, res, next) {
+
+  let step, implementation, file;
+  try {
+    step = await models.step.findOne({where:{workflowId: req.params.workflowId, position: req.params.position}});
+  } catch(error) {
+    error = "Error getting step for implementation: " + error;
+    logger.debug(error);
+    return res.status(500).send(error);
+  }
+  if (!step) return res.sendStatus(404);
+  try {
+    implementation = await models.implementation.findOne({where:{language:req.params.language,stepId:step.id}});
+  } catch(error) {
+    error = "Error getting implementation details: " + error;
+    logger.debug(error);
+    return res.status(500).send(error);
+  }
+  if (!implementation) return res.sendStatus(404);
+  try {
+    file = fs.readFileSync("uploads/" + step.workflowId + "/" + req.params.language + "/" + implementation.fileName, "utf8");
+  } catch(error) {
+    error = "Error reading implementation: " + error;
+    logger.debug(error);
+    return res.status(500).send(error);
+  }
+  if (!file) return res.sendStatus(500);
+  res.render("code",{title:"Phenoflow", implementation:file, language:implementation.language})
 
 });
 
