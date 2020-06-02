e9e2fceae5c7aa01f99be41d400c32b996b00e90
Martin Chapman <contact@martinchapman.co.uk>
2020-06-02 19:31:00 +0100

    Key length.

--- visualiser/git-server/certs/gen-domain-cert.sh
+++ visualiser/git-server/certs/gen-domain-cert.sh
@@ -3,7 +3,7 @@ set -eu
 name=pf
 org=pf-ca
 domain=git-server
-openssl genpkey -algorithm RSA -outform PEM -out "$domain"-key.pem
+openssl genpkey -algorithm RSA -outform PEM -out "$domain"-key.pem -pkeyopt rsa_keygen_bits:4096
 openssl req -new -key "$domain"-key.pem -out "$domain".csr -subj "/CN=$domain/O=$org"
 openssl x509 -req -in "$domain".csr -days 365 -outform PEM -CA "$name".pem -CAkey "$name".key -CAcreateserial \
     -extfile <(cat <<END
