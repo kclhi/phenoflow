46321233471dccb1d3357c365154a50c9d630714
Martin Chapman <contact@martinchapman.co.uk>
2021-11-04 16:56:29 +0000

    Support nested workflows in generated CWL

--- generator/api/routes.py
+++ generator/api/routes.py
@@ -21,26 +21,33 @@ async def generate(request):
         if (not "external" in steps[0]['type']): generatedWorkflowInputs["potentialCases"] = {'class':'File', 'path':'replaceMe.csv'};
 
         for step in steps:
-            # Send extension of last step output to signify workflow output
-            extension = None;
-            language = step['implementation']['language'];
+            
+            if('inputs' in step and 'outputs' in step): 
+              # Send extension of last step output to signify workflow output
+              extension = None;
+              language = step['implementation']['language'];
 
-            if (step==steps[len(steps) - 1]): extension = step['outputs'][0]['extension'];
+              if (step==steps[len(steps) - 1]): extension = step['outputs'][0]['extension'];
 
-            generatedWorkflow = workflow.createWorkflowStep(generatedWorkflow, step['position'], step['name'], step['type'], language, extension);
-            generatedWorkflowInputs["inputModule" + str(step['position'])] = {'class':'File', 'path':language + "/" + step['implementation']['fileName']};
+              generatedWorkflow = workflow.createWorkflowStep(generatedWorkflow, step['position'], step['name'], step['type'], language, extension);
+              generatedWorkflowInputs["inputModule" + str(step['position'])] = {'class':'File', 'path':language + "/" + step['implementation']['fileName']};
 
-            # ~MDC For now, we only assume one variable input to each step, the potential cases; and one variable output, the filtered potential cases.
-            if ( language == "python" ):
-                generatedStep = workflow.createPythonStep(step['name'], step['type'], step['doc'], step['inputs'][0]['doc'], step['outputs'][0]['extension'], step['outputs'][0]['doc']).export_string()
-            elif ( language == "knime" ):
-                generatedStep = workflow.createKNIMEStep(step['name'], step['type'], step['doc'], step['inputs'][0]['doc'], step['outputs'][0]['extension'], step['outputs'][0]['doc']).export_string();
-            elif ( language == "js" ):
-                generatedStep = workflow.createJSStep(step['name'], step['type'], step['doc'], step['inputs'][0]['doc'], step['outputs'][0]['extension'], step['outputs'][0]['doc']).export_string();
+              # ~MDC For now, we only assume one variable input to each step, the potential cases; and one variable output, the filtered potential cases.
+              if ( language == "python" ):
+                  generatedStep = workflow.createPythonStep(step['name'], step['type'], step['doc'], step['inputs'][0]['doc'], step['outputs'][0]['extension'], step['outputs'][0]['doc']).export_string()
+              elif ( language == "knime" ):
+                  generatedStep = workflow.createKNIMEStep(step['name'], step['type'], step['doc'], step['inputs'][0]['doc'], step['outputs'][0]['extension'], step['outputs'][0]['doc']).export_string();
+              elif ( language == "js" ):
+                  generatedStep = workflow.createJSStep(step['name'], step['type'], step['doc'], step['inputs'][0]['doc'], step['outputs'][0]['extension'], step['outputs'][0]['doc']).export_string();
+              else:
+                  # Handle unknown language
+                  generatedStep = "";
+
+              generatedSteps.append({"name":step['name'], "type":step['type'], "content":generatedStep, "fileName":step['implementation']['fileName']});
+            
             else:
-                # Handle unknown language
-                generatedStep = "";
-
-            generatedSteps.append({"name":step['name'], "type":step['type'], "content":generatedStep, "fileName":step['implementation']['fileName']});
+              nested_workflow_inputs = yaml.safe_load(step['implementation']['workflowInputs']);
+              for workflow_input in [nested_workflow_input for nested_workflow_input in nested_workflow_inputs if 'inputModule' in nested_workflow_input]: generatedWorkflowInputs["inputModule" + str(step['position']) + "-" + str(list(nested_workflow_inputs.keys()).index(workflow_input))] = {'class':'File', 'path':nested_workflow_inputs[workflow_input]['path']};
+              generatedWorkflow = workflow.createNestedWorkflowStep(generatedWorkflow, step['position'], step['implementation']);
 
     return JSONResponse({"workflow": yaml.dump(generatedWorkflow.get_dict(), default_flow_style=False), "steps": generatedSteps, "workflowInputs": yaml.dump(generatedWorkflowInputs, default_flow_style=False)})
