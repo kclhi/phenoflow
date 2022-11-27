1015256cd9b636a43a7e8360a6da17dda5567ab4
Martin Chapman <contact@martinchapman.co.uk>
2022-11-27 13:07:58 +0000

    Remove unused files

--- web/certs/gen-ca-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-org=pf-ca
-openssl genpkey -algorithm RSA -out pf.key -pkeyopt rsa_keygen_bits:4096
-openssl req -x509 -key pf.key -days 365 -outform PEM -out pf.pem -subj "/CN=$org/O=$org"
--- web/templates/read-potential-cases.template.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-with open('[PHENOTYPE]-potential-cases.csv', 'w', newline='') as file_out:
-    csv_writer = csv.DictWriter(file_out, ["patient-id","dob","codes","last-encounter"])
-    csv_writer.writeheader();
