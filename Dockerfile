99e30d911252db42cf0d2e339024b88cfcec7bef
Martin Chapman <contact@martinchapman.co.uk>
2023-04-21 18:43:05 +0100

    Update Docker config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM tiangolo/uvicorn-gunicorn-starlette:python3.7
+FROM tiangolo/uvicorn-gunicorn-starlette:python3.11
 COPY requirements.txt /app
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . /app
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from api.routes import app
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=3004, ssl_keyfile='certs/generator.key', ssl_certfile='certs/generator.crt');
+    uvicorn.run(app, host='0.0.0.0', port=3004, ssl_keyfile='certs/generator-webapp-1.key', ssl_certfile='certs/generator-webapp-1.crt');
